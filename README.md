01f08e966ae0c252a86ae79e22d14ddf9ca884ae
Jun Wei <junweilee72@gmail.com>
2026-06-26 09:23:43 +0800

    extra plot utils

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 JIT compiled Monte Carlo simulation of the [Hanbury Brown & Twiss experiment](https://mpl.mpg.de/fileadmin/user_upload/Chekhova_Research_Group/Lecture_5_3.pdf), using a pair of nitrogen vacancy centers and lasers (to simulate probabilistic double, single, zero photon emission per pulse). The result is the second order correlation function at $\tau$ = 0 for each seed and efficiency pair.
-
-![image](./assets/Figure_1.png)
--- src/plot_utils.py
+++ src/plot_utils.py
@@ -8,3 +8,30 @@ def plot_coincidence_events(taus: np.ndarray, bins: int) -> None:
     plt.xlabel("tau")
     plt.ylabel("coincidence events")
     plt.show()
+
+
+def plot_via_histogram(hist: np.ndarray, edges: np.ndarray) -> None:
+    plt.stairs(hist, edges, fill=True, color="y")
+    plt.title("tau - coincidence events")
+    plt.xlabel("tau")
+    plt.ylabel("coincidence events")
+    plt.show()
+
+
+def plot_adjacent(
+    taus: np.ndarray, bins: int, hist: np.ndarray, edges: np.ndarray
+) -> None:
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+    ax1.hist(taus, bins)
+    ax1.set_title("tau - coincidence events (pre np.histogram)")
+    ax1.set_xlabel("tau")
+    ax1.set_ylabel("coincidence events")
+
+    ax2.stairs(hist, edges, fill=True, color="y")
+    ax2.set_title("tau - coincidence events (post np.histogram)")
+    ax2.set_xlabel("tau")
+    ax2.set_ylabel("coincidence events")
+
+    plt.tight_layout()
+    plt.show()
